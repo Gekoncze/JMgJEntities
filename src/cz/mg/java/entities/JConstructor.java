c3ba1fc628727351c92078253a5d0afa85c30a34
mg <gekoncze@seznam.cz>
2025-09-07 19:10:31 +0200

    added support for compact constructors

--- src/cz/mg/java/entities/JConstructor.java
+++ src/cz/mg/java/entities/JConstructor.java
@@ -1,11 +1,28 @@
 package cz.mg.java.entities;
 
 import cz.mg.annotations.classes.Entity;
+import cz.mg.annotations.requirement.Optional;
+import cz.mg.annotations.requirement.Required;
+import cz.mg.annotations.storage.Commons;
+import cz.mg.annotations.storage.Parts;
+import cz.mg.annotations.storage.Value;
+import cz.mg.annotations.storage.Values;
 import cz.mg.collections.list.List;
 import cz.mg.java.entities.bounds.JBound;
+import cz.mg.java.entities.interfaces.JAnnotable;
+import cz.mg.java.entities.interfaces.JCommentable;
+import cz.mg.java.entities.interfaces.JModifiable;
 import cz.mg.token.Token;
 
-public @Entity class JConstructor extends JMethod {
+public @Entity class JConstructor implements JEntity, JAnnotable, JModifiable, JCommentable {
+    private String comment;
+    private List<JAnnotation> annotations = new List<>();
+    private List<JModifier> modifiers = new List<>();
+    private List<JBound> bounds = new List<>();
+    private String name;
+    private List<JVariable> input = new List<>();
+    private List<Token> implementation;
+
     public JConstructor() {
     }
 
@@ -14,20 +31,85 @@ public @Entity class JConstructor extends JMethod {
         List<JAnnotation> annotations,
         List<JModifier> modifiers,
         List<JBound> bounds,
-        JType output,
         String name,
         List<JVariable> input,
         List<Token> implementation
     ) {
-        super(
-            comment,
-            annotations,
-            modifiers,
-            bounds,
-            output,
-            name,
-            input,
-            implementation
-        );
+        this.comment = comment;
+        this.annotations = annotations;
+        this.modifiers = modifiers;
+        this.bounds = bounds;
+        this.name = name;
+        this.input = input;
+        this.implementation = implementation;
+    }
+
+    @Override
+    @Optional @Value
+    public String getComment() {
+        return comment;
+    }
+
+    @Override
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    @Required @Parts
+    public List<JAnnotation> getAnnotations() {
+        return annotations;
+    }
+
+    @Override
+    public void setAnnotations(List<JAnnotation> annotations) {
+        this.annotations = annotations;
+    }
+
+    @Override
+    @Required @Values
+    public List<JModifier> getModifiers() {
+        return modifiers;
+    }
+
+    @Override
+    public void setModifiers(List<JModifier> modifiers) {
+        this.modifiers = modifiers;
+    }
+
+    @Required @Commons
+    public List<JBound> getBounds() {
+        return bounds;
+    }
+
+    public void setBounds(List<JBound> bounds) {
+        this.bounds = bounds;
+    }
+
+    @Required @Value
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Optional @Parts
+    public List<JVariable> getInput() {
+        return input;
+    }
+
+    public void setInput(List<JVariable> input) {
+        this.input = input;
+    }
+
+    @Required @Commons
+    public List<Token> getImplementation() {
+        return implementation;
+    }
+
+    public void setImplementation(List<Token> implementation) {
+        this.implementation = implementation;
     }
 }
